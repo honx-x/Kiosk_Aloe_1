90eb872dbcd53579826289e251ac6480b7aa9ff9
Hi_HONG <Hi_HONG@DESKTOP-60RNGHR>
2020-11-16 18:19:23 +0900

    201116

--- Kiosk_ex1/Kiosk_WorkOrder_Status.Designer.cs
+++ Kiosk_ex1/Kiosk_WorkOrder_Status.Designer.cs
@@ -32,23 +32,14 @@
             this.panel_main = new System.Windows.Forms.Panel();
             this.layout_menu1_1 = new System.Windows.Forms.TableLayoutPanel();
             this.label1 = new System.Windows.Forms.Label();
-            this.listView1 = new System.Windows.Forms.ListView();
             this.label2 = new System.Windows.Forms.Label();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column10 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.gridView_CL = new System.Windows.Forms.DataGridView();
+            this.gridView_OL = new System.Windows.Forms.DataGridView();
             label3 = new System.Windows.Forms.Label();
             this.panel_main.SuspendLayout();
             this.layout_menu1_1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView_CL)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView_OL)).BeginInit();
             this.SuspendLayout();
             // 
             // label3
@@ -65,7 +56,6 @@
             label3.TabIndex = 5;
             label3.Text = "작업현황";
             label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // panel_main
             // 
@@ -83,9 +73,9 @@
             this.layout_menu1_1.ColumnCount = 1;
             this.layout_menu1_1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.layout_menu1_1.Controls.Add(this.label1, 0, 0);
-            this.layout_menu1_1.Controls.Add(this.listView1, 0, 1);
             this.layout_menu1_1.Controls.Add(this.label2, 0, 2);
-            this.layout_menu1_1.Controls.Add(this.dataGridView1, 0, 3);
+            this.layout_menu1_1.Controls.Add(this.gridView_CL, 0, 3);
+            this.layout_menu1_1.Controls.Add(this.gridView_OL, 0, 1);
             this.layout_menu1_1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.layout_menu1_1.Location = new System.Drawing.Point(0, 150);
             this.layout_menu1_1.Name = "layout_menu1_1";
@@ -94,6 +84,7 @@
             this.layout_menu1_1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.layout_menu1_1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
             this.layout_menu1_1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.layout_menu1_1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
             this.layout_menu1_1.Size = new System.Drawing.Size(1129, 795);
             this.layout_menu1_1.TabIndex = 6;
             // 
@@ -111,19 +102,6 @@
             this.label1.Text = "- 작업지시리스트 (Work Order List)";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // listView1
-            // 
-            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.listView1.HideSelection = false;
-            this.listView1.Location = new System.Drawing.Point(10, 60);
-            this.listView1.Margin = new System.Windows.Forms.Padding(10);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(1109, 327);
-            this.listView1.TabIndex = 1;
-            this.listView1.UseCompatibleStateImageBehavior = false;
-            // 
             // label2
             // 
             this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
@@ -138,76 +116,33 @@
             this.label2.Text = "- 작업완료내역 (Work Close List)";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // dataGridView1
+            // gridView_CL
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.Column1,
-            this.Column2,
-            this.Column3,
-            this.Column4,
-            this.Column5,
-            this.Column6,
-            this.Column7,
-            this.Column8,
-            this.Column9,
-            this.Column10});
-            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dataGridView1.Location = new System.Drawing.Point(3, 450);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowTemplate.Height = 23;
-            this.dataGridView1.Size = new System.Drawing.Size(1123, 342);
-            this.dataGridView1.TabIndex = 4;
+            this.gridView_CL.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.gridView_CL.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.gridView_CL.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridView_CL.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gridView_CL.Location = new System.Drawing.Point(3, 450);
+            this.gridView_CL.Name = "gridView_CL";
+            this.gridView_CL.RowTemplate.Height = 23;
+            this.gridView_CL.Size = new System.Drawing.Size(1123, 342);
+            this.gridView_CL.TabIndex = 4;
             // 
-            // Column1
+            // gridView_OL
             // 
-            this.Column1.HeaderText = "지시번호";
-            this.Column1.Name = "Column1";
-            // 
-            // Column2
-            // 
-            this.Column2.HeaderText = "상태";
-            this.Column2.Name = "Column2";
-            // 
-            // Column3
-            // 
-            this.Column3.HeaderText = "타각넘버";
-            this.Column3.Name = "Column3";
-            // 
-            // Column4
-            // 
-            this.Column4.HeaderText = "파트넘버";
-            this.Column4.Name = "Column4";
-            // 
-            // Column5
-            // 
-            this.Column5.HeaderText = "파트네임";
-            this.Column5.Name = "Column5";
-            // 
-            // Column6
-            // 
-            this.Column6.HeaderText = "공정코드";
-            this.Column6.Name = "Column6";
-            // 
-            // Column7
-            // 
-            this.Column7.HeaderText = "공정명";
-            this.Column7.Name = "Column7";
-            // 
-            // Column8
-            // 
-            this.Column8.HeaderText = "지시일자";
-            this.Column8.Name = "Column8";
-            // 
-            // Column9
-            // 
-            this.Column9.HeaderText = "긴급여부";
-            this.Column9.Name = "Column9";
-            // 
-            // Column10
-            // 
-            this.Column10.HeaderText = "비고";
-            this.Column10.Name = "Column10";
+            this.gridView_OL.AllowUserToAddRows = false;
+            this.gridView_OL.AllowUserToDeleteRows = false;
+            this.gridView_OL.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.gridView_OL.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.Sunken;
+            this.gridView_OL.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridView_OL.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gridView_OL.Location = new System.Drawing.Point(3, 53);
+            this.gridView_OL.MultiSelect = false;
+            this.gridView_OL.Name = "gridView_OL";
+            this.gridView_OL.ReadOnly = true;
+            this.gridView_OL.RowTemplate.Height = 23;
+            this.gridView_OL.Size = new System.Drawing.Size(1123, 341);
+            this.gridView_OL.TabIndex = 5;
             // 
             // Kiosk_WorkOrder_Status
             // 
@@ -220,7 +155,8 @@
             this.panel_main.ResumeLayout(false);
             this.layout_menu1_1.ResumeLayout(false);
             this.layout_menu1_1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView_CL)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView_OL)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -228,19 +164,9 @@
         #endregion
         private System.Windows.Forms.Panel panel_main;
         private System.Windows.Forms.TableLayoutPanel layout_menu1_1;
-        private System.Windows.Forms.ListView listView1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column8;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column9;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column10;
+        private System.Windows.Forms.DataGridView gridView_CL;
+        private System.Windows.Forms.DataGridView gridView_OL;
     }
 }
--- Kiosk_ex1/Kiosk_WorkOrder_Status.cs
+++ Kiosk_ex1/Kiosk_WorkOrder_Status.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Data;
 using System.Data.SqlClient;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace Kiosk_ex1
@@ -22,42 +23,113 @@ namespace Kiosk_ex1
             DayOfWeek today = DateTime.Today.DayOfWeek;
             //this.label_date.Text = NowDate + "(" + today + ")";
 
-            ListView1();
+            //InitGridControl();
+            GridView_OL();
+            GridView_CL();
 
 
+        }
 
+        private void GridView_CL()
+        {
 
             SqlConnection sqlcon = new SqlConnection("Server=192.168.0.70; Database=ARTECH; uid=SA; pwd=hidata2312357!");
+            SqlCommand cmd = new SqlCommand();
             sqlcon.Open();
 
 
+            cmd.Connection = sqlcon;
+            cmd.CommandText = "SELECT * FROM dbo.PdWorkOrder";
 
-        }
+
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            da.Fill(ds, "Work_table");
+
+            gridView_OL.DataSource = ds;
+            gridView_OL.DataMember = "Work_table";
 
 
-        private void ListView1()
-        {
-            listView1.View = View.Details;
-            //listView1.size
-
-            listView1.BeginUpdate();
-            listView1.Columns.Add("지시번호");
-            listView1.Columns.Add("타각넘버");
-            listView1.Columns.Add("PART NAME");
-            listView1.Columns.Add("공정명");
-            listView1.Columns.Add("지시일자");
-            listView1.Columns.Add("긴급");
-            listView1.Columns.Add("진행상태");
-
-            listView1.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent); // 컬럼 내용별 오토사이즈
-            listView1.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize); // 컬럼 헤더제목 오토사이즈
-
+            /*
+            gridView_OL.Columns["WorkOrderNo"].HeaderText = "작업지시번호";
+            gridView_OL.Columns["OrderStatus"].HeaderText = "상태        ";
+            gridView_OL.Columns["EngravingNo"].HeaderText = "타각넘버    ";
+            gridView_OL.Columns["ItemSeq"].HeaderText = "제품일련번호";
+            gridView_OL.Columns["ItemNo"].HeaderText = "PART NO     ";
+            gridView_OL.Columns["ItemNm"].HeaderText = "PART NAME   ";
+            gridView_OL.Columns["ProcSeq"].HeaderText = "공정일련번호";
+            gridView_OL.Columns["ProcCd"].HeaderText = "공정코드    ";
+            gridView_OL.Columns["ProcNm"].HeaderText = "공정명      ";
+            gridView_OL.Columns["OrderDt"].HeaderText = "지시일자    ";
+            gridView_OL.Columns["EmergencyYn"].HeaderText = "긴급여부    ";
+            gridView_OL.Columns["Remark"].HeaderText = "비고        ";
+            */
 
             
+            gridView_OL.Columns[0].HeaderText = "작업지시번호";
+            gridView_OL.Columns[1].HeaderText = "상태";
+            gridView_OL.Columns[2].HeaderText = "타각넘버";
+            gridView_OL.Columns[3].HeaderText = "제품일련번호";
+            gridView_OL.Columns[4].HeaderText = "PART NO";
+            gridView_OL.Columns[5].HeaderText = "지시일자";
+            gridView_OL.Columns[6].HeaderText = "긴급여부";
+            gridView_OL.Columns[7].HeaderText = "삭제가능여부";
+            gridView_OL.Columns[8].HeaderText = "지시자";
+            gridView_OL.Columns[9].HeaderText = "지시일자";
+            gridView_OL.Columns[10].HeaderText = "수정자";
+            gridView_OL.Columns[11].HeaderText = "수정일자";
+            gridView_OL.Columns[12].HeaderText = "비고";
+            
+
+            this.gridView_OL.ColumnHeadersDefaultCellStyle.Font = new Font("돋움", 15, FontStyle.Bold);
+            this.gridView_OL.DefaultCellStyle.Font = new Font("돋움", 12, FontStyle.Regular);
+            this.gridView_OL.ColumnHeadersDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            this.gridView_OL.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+
+            
+
+
         }
 
-        private void label3_Click(object sender, EventArgs e)
+        private void GridView_OL()
         {
+            SqlConnection sqlcon = new SqlConnection("Server=192.168.0.70; Database=ARTECH; uid=SA; pwd=hidata2312357!");
+            SqlCommand cmd = new SqlCommand();
+            sqlcon.Open();
+
+
+            cmd.Connection = sqlcon;
+            cmd.CommandText = "SELECT * FROM dbo.PdWorkOrder";
+
+
+            SqlDataAdapter da = new SqlDataAdapter(cmd); 
+            DataSet ds = new DataSet();
+            da.Fill(ds, "Work_table");
+
+            gridView_CL.DataSource = ds;
+            gridView_CL.DataMember = "Work_table";
+
+            this.gridView_CL.ColumnHeadersDefaultCellStyle.Font = new Font("돋움", 15, FontStyle.Bold);
+            this.gridView_CL.DefaultCellStyle.Font = new Font("돋움", 12, FontStyle.Regular);
+
+        }
+
+
+        private void InitGridControl()
+        {
+
+            gridView_OL.Columns["WorkOrderNo"].HeaderText = "작업지시번호";
+            gridView_OL.Columns["OrderStatus"].HeaderText = "상태        ";
+            gridView_OL.Columns["EngravingNo"].HeaderText = "타각넘버    ";
+            gridView_OL.Columns["ItemSeq    "].HeaderText = "제품일련번호";
+            gridView_OL.Columns["ItemNo     "].HeaderText = "PART NO     ";
+            gridView_OL.Columns["ItemNm     "].HeaderText = "PART NAME   ";
+            gridView_OL.Columns["ProcSeq    "].HeaderText = "공정일련번호";
+            gridView_OL.Columns["ProcCd     "].HeaderText = "공정코드    ";
+            gridView_OL.Columns["ProcNm     "].HeaderText = "공정명      ";
+            gridView_OL.Columns["OrderDt    "].HeaderText = "지시일자    ";
+            gridView_OL.Columns["EmergencyYn"].HeaderText = "긴급여부    ";
+            gridView_OL.Columns["Remark     "].HeaderText = "비고        ";
 
         }
     }
